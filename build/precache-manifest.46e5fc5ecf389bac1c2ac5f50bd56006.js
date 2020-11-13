26d887c52651a6e6045927bf9c4f3b68256da057
unknown <petliashenko@ukr.net>
2020-11-13 15:33:38 +0200

    travis2

---
+++ build/precache-manifest.46e5fc5ecf389bac1c2ac5f50bd56006.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "223117a654706265ae008165d752a672",
+    "url": "/reactTickTackToe/index.html"
+  },
+  {
+    "revision": "6c5932d2c9e2365b9762",
+    "url": "/reactTickTackToe/static/css/main.3fa7586b.chunk.css"
+  },
+  {
+    "revision": "35fd313524afd6734728",
+    "url": "/reactTickTackToe/static/js/2.a1137f20.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/reactTickTackToe/static/js/2.a1137f20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c5932d2c9e2365b9762",
+    "url": "/reactTickTackToe/static/js/main.9f90d2d3.chunk.js"
+  },
+  {
+    "revision": "7d281c9b9e7550f695a3",
+    "url": "/reactTickTackToe/static/js/runtime-main.a28d1433.js"
+  }
+]);
