1c3d28425e218eefb66db4f4f64c723afd082482
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-11-13 15:06:57 +0000

    Deploy AndreyPetlyashenko/reactTickTackToe to github.com/AndreyPetlyashenko/reactTickTackToe.git:gh-pages

---
+++ precache-manifest.d4cbfa41cd923acfa984b5599f9613f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83a0b840c9be42de8d55e33bbd750b91",
+    "url": "/reactTickTackToe/index.html"
+  },
+  {
+    "revision": "abaa97a0573d137637f2",
+    "url": "/reactTickTackToe/static/css/main.b1aff90b.chunk.css"
+  },
+  {
+    "revision": "35fd313524afd6734728",
+    "url": "/reactTickTackToe/static/js/2.a1137f20.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/reactTickTackToe/static/js/2.a1137f20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abaa97a0573d137637f2",
+    "url": "/reactTickTackToe/static/js/main.7c32af92.chunk.js"
+  },
+  {
+    "revision": "7d281c9b9e7550f695a3",
+    "url": "/reactTickTackToe/static/js/runtime-main.a28d1433.js"
+  }
+]);
